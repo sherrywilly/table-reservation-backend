19229ef6c2be03408b90c43a9b1aad0824a7c7d3
sherrywilly <sherrywilson521@gmail.com>
2021-07-13 20:50:50 +0530

    updateted small bugs

--- deletemod/views.py
+++ deletemod/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import logout
 from django.shortcuts import redirect, render
 from restaurant.models import *
 
@@ -8,24 +9,29 @@ def deleteRest(request, pk):
     if request.method == "POST":
         Restaurant.objects.get(id=id).delete()
 
-    return redirect(reverse())
+    return redirect(reverse('restaurant'))
 
 
 def Catedelete(request, pk):
     if request.method == "POST":
         Category.objects.get(id=id).delete()
 
-    return redirect(reverse())
+    return redirect(reverse("cat-list-rest"))
 
 
 def RestCatdelete(request, pk):
     if request.method == "POST":
         RestCategory.objects.get(id=id).delete()
 
-    return redirect(reverse())
+    return redirect(reverse('restcatlist'))
 
 
 def ItemDelete(request, pk):
     if request.method == "POST":
         Item.objects.get(id=id).delete()
-    return redirect(reverse())
+    return redirect(reverse("rest-item-list"))
+
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse('login-view'))
